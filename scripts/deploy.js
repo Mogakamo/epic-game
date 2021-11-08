071dd59c53cfe04af77568b6b142c2ee84904e48
Mogakamo <mogaka.amo254@gmail.com>
2021-11-08 09:28:21 +0000

    commit

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,50 +1,47 @@
 const main = async () => {
-  const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
-  const gameContract = await gameContractFactory.deploy(
-    ["Leo", "Aang", "Pikachu"],
-    [
-      "https://i.imgur.com/pKd5Sdk.png",
-      "https://i.imgur.com/xVu4vFL.png",
-      "https://i.imgur.com/u7T87A6.png",
-    ],
-    [100, 200, 300],
-    [100, 50, 25],
-      "Elon Musk", // Boss name
-      "https://i.imgur.com/AksR0tt.png",
-      10000, // Boss hp
-      50 // Boss attack damage
-  );
-  await gameContract.deployed();
-  console.log("Contract deployed to:", gameContract.address);
+    const gameContractFactory = await hre.ethers.getContractFactory(
+      "MyEpicGame"
+    );
+    const gameContract = await gameContractFactory.deploy(
+        ["Leo", "Aang", "Pikachu"],
+        ["https://i.imgur.com/pKd5Sdk.png",
+            "https://i.imgur.com/xVu4vFL.png",
+            "https://i.imgur.com/WMB6g9u.png"],
+        [100, 200, 300],
+        [100, 50, 25],
+        "Elon Musk", //Boss name
+        "https://i.imgur.com/AksR0tt.png",
+        10000, //Boss hp
+        50 // Boss attack damage
+    );
+    await gameContract.deployed();
+    console.log("Contract deployed to:", gameContract.address)
+        
+    let txn;
 
-  let txn;
-  txn = await gameContract.mintCharacterNFT(0);
-  await txn.wait();
-  console.log("Minted NFT #1");
+    txn = await gameContract.mintCharacterNFT(2);
+    await txn.wait();
 
-  txn = await gameContract.mintCharacterNFT(1);
-  await txn.wait();
-  console.log("Minted NFT #2");
+    txn = await gameContract.attackBoss();
+    await txn.wait();
 
-  txn = await gameContract.mintCharacterNFT(2);
-  await txn.wait();
-  console.log("Minted NFT #3");
+    txn = await gameContract.attackBoss();
+    await txn.wait();
 
-  txn = await gameContract.mintCharacterNFT(1);
-  await txn.wait();
-  console.log("Minted NFT #4");
+    // Get the value of the NFT's URI
+    let returnedTokenUri = await gameContract.tokenURI(1);
+    console.log("Token URI:", returnedTokenUri);
 
-  console.log("Done deploying and minting!");
 };
 
 const runMain = async () => {
-  try {
-    await main();
-    process.exit(0);
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
+    try {
+        await main();
+        process.exit(0);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
 };
 
 runMain();
